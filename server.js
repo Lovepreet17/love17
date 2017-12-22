4b9c2cd00a3498e78f4b535132bb0a0d7c78f752
Lovepreet17 <alovepreet17@gmail.com>
2017-12-22 14:39:32 -0800

    code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Love!'))
+app.use(express.static('./'))
 
 app.listen(8080, () => console.log('Example app listening on port 3000!'))
